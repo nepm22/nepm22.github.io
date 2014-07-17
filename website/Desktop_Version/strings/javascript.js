8c72e393eb1d3e5934e1e362a37cdb821ce5f605
nepm22 <noah.milman2@gmail.com>
2014-07-16 20:45:59 -0700

    final files

---
+++ website/Desktop_Version/strings/javascript.js
@@ -0,0 +1,17 @@
+<script language="javascript">     
+<script>
+<FORM>
+<INPUT TYPE="button" onClick="parent.location='file:///Users/NoahMilman/Desktop/website/raquets.html'">
+</FORM>
+<script type="text/javascript">
+function redirect() {
+	window.location = "file:///Users/NoahMilman/Desktop/website/Raquets/raquets.html"
+
+}
+function redirecttwo() {
+	window.location = "file:///Users/NoahMilman/Desktop/website/Shoes/shoes.html"
+
+}
+
+
+</script>
